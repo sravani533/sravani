494c75f8866c14ed8226a206f195ee0e5b55d254
sravani533 <76991914+sravani533@users.noreply.github.com>
2024-12-16 17:04:34 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,8 @@ secret_key = "aws_secret_key"
 }
 
 resource "aws_instance" "key" {
-ami = ""
+ami = "ami-0166fe664262f664c"
+key_name = "sr"
 instance_type = "t2.micro"
 vpc_security_group_ids = [aws_security_group.demo-sg.id]
 tags = {
